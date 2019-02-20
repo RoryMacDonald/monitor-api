070968801d53a1bc883fb0cfbcabcc278f8b53fc
Faissal Bensefia <faissal@madetech.com>
2019-02-20 13:41:37 +0000

    Add basic ff project schema

--- spec/unit/ui/gateway/in_memory_project_schema_spec.rb
+++ spec/unit/ui/gateway/in_memory_project_schema_spec.rb
@@ -7,6 +7,18 @@ describe UI::Gateway::InMemoryProjectSchema do
     expect(schema).not_to be_nil
   end
 
+  it 'Returns a schema when finding by type ac' do
+    gateway = described_class.new
+    schema = gateway.find_by(type: 'ac')
+    expect(schema).not_to be_nil
+  end
+
+  it 'Returns a schema when finding by type ff' do
+    gateway = described_class.new
+    schema = gateway.find_by(type: 'ff')
+    expect(schema).not_to be_nil
+  end
+
   it 'Returns nil when searching for a non existing type' do
     gateway = described_class.new
     schema = gateway.find_by(type: 'cats 4 lyf')
