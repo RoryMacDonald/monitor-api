1131013ce46764d615ec8ffb43147028285e7e3b
Faissal Bensefia <faissal@madetech.com>
2019-02-26 11:34:50 +0000

    Provide reference spike for implementation of live PCS

--- lib/homes_england/gateway/pcs_gateway.rb
+++ lib/homes_england/gateway/pcs_gateway.rb
@@ -17,8 +17,10 @@ class HomesEngland::Gateway::Pcs
   end
 
   def request_pcs_data(bid_id, endpoint, pcs_key)
-    pcs_endpoint = Net::HTTP.new(@pcs_url)
-    request = Net::HTTP::Get.new("/project/#{ERB::Util.url_encode(bid_id)}#{endpoint}")
+    pcs_key = "REAL API KEY"
+    pcs_endpoint = Net::HTTP.new(@pcs_url, 443)
+    pcs_endpoint.use_ssl = true
+    request = Net::HTTP::Get.new("/pcs-api/v1/Projects/#{ERB::Util.url_encode(ERB::Util.url_encode(bid_id))}#{endpoint}")
     request['Authorization'] = "Bearer #{pcs_key}"
     response = pcs_endpoint.request(request)
     Common::DeepSymbolizeKeys.to_symbolized_hash(JSON.parse(response.body))
