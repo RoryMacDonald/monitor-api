8cfcdf17fd92b5e013ec8f72121412a367be3666
Faissal Bensefia <faissal@madetech.com>
2019-02-28 11:09:25 +0000

    Add /claim/create

--- lib/delivery_mechanism/web_routes.rb
+++ lib/delivery_mechanism/web_routes.rb
@@ -84,20 +84,6 @@ module DeliveryMechanism
       end
     end
 
-    post '/return/create' do
-      guard_access env, params, request do |request_hash|
-        return_id = @dependency_factory.get_use_case(:ui_create_return).execute(
-          project_id: request_hash[:project_id],
-          data: request_hash[:data]
-        )
-
-        response.tap do |r|
-          r.body = { id: return_id[:id] }.to_json
-          r.status = 201
-        end
-      end
-    end
-
     post '/return/submit' do
       guard_access env, params, request do |request_hash|
         DeliveryMechanism::Controllers::PostSubmitReturn.new(
