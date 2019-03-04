e20d01183ef0f62ed307918561a9fc751c3d06d6
Christopher Parsons <mail@christopherparsons.me>
2019-03-04 17:03:56 +0000

    Add HIF Grant Expenditure tab to Forward Funding

--- lib/ui/use_case/convert_core_ff_project.rb
+++ lib/ui/use_case/convert_core_ff_project.rb
@@ -12,6 +12,7 @@ class UI::UseCase::ConvertCoreFFProject
     convert_land_owenership
     convert_procurement
     convert_demolition_and_remediation
+    convert_hif_grant_expenditure
 
     @converted_project
   end
@@ -52,6 +53,11 @@ class UI::UseCase::ConvertCoreFFProject
     return if @project[:demolitionRemediation].nil?
 
     @converted_project[:demolitionRemediation] = @project[:demolitionRemediation]
+  end
 
+  def convert_hif_grant_expenditure
+    return if @project[:hifGrantExpenditure].nil?
+
+    @converted_project[:hifGrantExpenditure] = @project[:hifGrantExpenditure]
   end
 end
