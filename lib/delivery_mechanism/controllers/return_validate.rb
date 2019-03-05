da3f7fdc81218a40e475a1c3bdedff6ca5c66d10
Faissal Bensefia <faissal@madetech.com>
2019-03-05 15:03:43 +0000

    Add claim validation webroute

---
+++ lib/delivery_mechanism/controllers/return_validate.rb
@@ -0,0 +1,19 @@
+require_relative '../web_routes.rb'
+
+DeliveryMechanism::WebRoutes.post '/return/validate' do
+  guard_access env, params, request do |request_hash|
+    return 400 if invalid_validation_hash(request_hash: request_hash)
+
+    validate_response = @dependency_factory.get_use_case(:ui_validate_return).execute(
+      type: request_hash[:type],
+      return_data: request_hash[:data]
+    )
+
+    response.status = 200
+    response.body = {
+      valid: validate_response[:valid],
+      invalidPaths: validate_response[:invalid_paths],
+      prettyInvalidPaths: validate_response[:pretty_invalid_paths]
+    }.to_json
+  end
+end
