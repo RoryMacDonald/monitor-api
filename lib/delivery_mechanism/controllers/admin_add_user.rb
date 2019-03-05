84dbf384da71e278daccb2ae118158975d486a85
Christopher Parsons <mail@christopherparsons.me>
2019-03-05 16:27:47 +0000

    Fix issues with http web route and wrap in tests

--- lib/delivery_mechanism/controllers/admin_add_user.rb
+++ lib/delivery_mechanism/controllers/admin_add_user.rb
@@ -1,26 +1,12 @@
+# frozen_string_literal: true
+
 require_relative '../web_routes.rb'
 
-DeliveryMechanism::WebRoutes.post '/project/:id/add_users' do
-  guard_access env, params, request do |request_hash|
-    begin
-      project_id = Integer(params[:id])
-    rescue ArgumentError
-      return 400
-    end
-    user_emails = request_hash[:users]
-
-    return 400 unless user_emails.instance_of? Array
-
-    user_emails.each do |user_info|
-      @dependency_factory.get_use_case(:add_user_to_project).execute(
-        {
-          email: user_info[:email],
-          role: user_info[:role],
-          project_id: project_id
-        }
-      )
-    end
-    response.status = 200
-    response
+DeliveryMechanism::WebRoutes.post '/project/admin/:id/add_users' do
+  guard_admin_access env, params, request do |request_hash|
+    controller = DeliveryMechanism::Controllers::PostProjectToUsers.new(
+      add_user_to_project: @dependency_factory.get_use_case(:add_user_to_project)
+    )
+    controller.execute(params, request_hash, response)
   end
 end
--- spec/web_routes/add_users_to_project_spec.rb
+++ spec/web_routes/add_users_to_project_spec.rb
@@ -135,3 +135,121 @@ describe 'Adding users to a project' do
     end
   end
 end
+
+describe 'Admin adding users to a project' do
+  ENV['ADMIN_HTTP_API_KEY'] = 'supersecret'
+
+  def set_correct_auth_header
+    header 'API_KEY', ENV['ADMIN_HTTP_API_KEY']
+  end
+
+  def set_incorrect_auth_header
+    header 'API_KEY', ENV['ADMIN_HTTP_API_KEY'] + 'make_key_invalid'
+  end
+
+  context 'when no authorization provided in a header' do
+    let(:body) { { users: [{ email: 'person1@mt.com' }] } }
+
+    it 'returns 400' do
+      post('project/admin/1/add_users', body.to_json)
+      expect(last_response.status).to eq(401)
+    end
+  end
+
+  context 'when correct authorization provided' do
+    before do
+      stub_const(
+        'LocalAuthority::UseCase::CheckApiKey',
+        double(new: double(execute: {valid: true}))
+        )
+
+      set_correct_auth_header
+    end
+
+    context 'when request body is invalid' do
+      context 'does not have users key' do
+        let(:invalid_body) { { invalid_key: [{ email: 'person1@mt.com' }] } }
+
+        it 'returns 400' do
+          post('project/admin/1/add_users', invalid_body.to_json)
+          expect(last_response.status).to eq(400)
+        end
+      end
+
+      context 'users key is not an array' do
+        let(:invalid_body) { { users: 'person1@mt.com' } }
+
+        it 'returns 400' do
+          post('project/admin/1/add_users', invalid_body.to_json)
+          expect(last_response.status).to eq(400)
+        end
+      end
+    end
+    context 'when request body is valid' do
+      let(:add_user_to_project_usecase_spy) { spy }
+      let(:valid_request_body) { { users: [{ email: 'mt@mt.com' }] } }
+
+      before do
+        stub_const(
+          'HomesEngland::UseCase::AddUserToProject',
+          double(new: add_user_to_project_usecase_spy)
+        )
+      end
+
+      it 'returns 200' do
+        post('project/admin/1/add_users', valid_request_body.to_json)
+        expect(last_response.status).to eq(200)
+      end
+
+      context 'users array has no entries' do
+        let(:body) { { users: [] } }
+
+        it 'returns 200' do
+          post('project/admin/1/add_users', body.to_json)
+          expect(last_response.status).to eq(200)
+        end
+      end
+
+      context 'it adds a single user' do
+        example 'example 1' do
+          request_body = { users: [{ email: 'mt1@mt1.com', role: 'S151' }] }
+          post('project/admin/33/add_users', request_body.to_json)
+          expect(add_user_to_project_usecase_spy).to have_received(:execute).with(
+            project_id: 33,
+            role: 'S151',
+            email: 'mt1@mt1.com'
+          )
+        end
+
+
+        example 'example 2' do
+          request_body = { users: [{ email: 'cat@mouse.com' }] }
+          post('project/admin/24/add_users', request_body.to_json)
+          expect(add_user_to_project_usecase_spy).to have_received(:execute).with(
+            project_id: 24,
+            role: nil,
+            email: 'cat@mouse.com'
+          )
+        end
+      end
+
+      context 'when each entry in the body is non-empty' do
+        let(:request_body) { { users: [{ email: 'mt1@mt1.com', role: 'Homes England' }, { email: 'mt2@mt2.com', role: 'Local Authority' }] } }
+
+        it 'execute AddUserToProject use case for each valid email' do
+          post('project/admin/33/add_users', request_body.to_json)
+          expect(add_user_to_project_usecase_spy).to have_received(:execute).with(
+            project_id: 33,
+            role: 'Homes England',
+            email: 'mt1@mt1.com'
+          )
+          expect(add_user_to_project_usecase_spy).to have_received(:execute).with(
+            project_id: 33,
+            role: 'Local Authority',
+            email: 'mt2@mt2.com'
+          )
+        end
+      end
+    end
+  end
+end
