7e108f753674e71e2e8a16a59518260dc7d9361f
Faissal Bensefia <faissal@madetech.com>
2019-03-06 10:00:46 +0000

    Return nil if pcs project does not exist

--- spec/unit/homes_england/gateway/pcs_gateway_spec.rb
+++ spec/unit/homes_england/gateway/pcs_gateway_spec.rb
@@ -1,30 +1,72 @@
-describe HomesEngland::Gateway::Pcs do
-  context 'Example 1' do
-    let(:pcs_secret) { '1119003331' }
-    let(:auth_token) { JWT.encode({}, pcs_secret, 'HS512') }
-    let(:pcs_domain) { 'https://meow.cat' }
-    let(:pcs_overview_request) do
-      stub_request(
-        :get,
-        "#{pcs_domain}/pcs-api/v1/Projects/HIF%252FMV%252F255"
-      ).to_return(
-        status: 200,
-        body: {
-          ProjectManager: "Ed",
-          Sponsor: "FIS"
-        }.to_json
-      ).with(
-        headers: {'Authorization' => "Bearer #{auth_token}" }
-      )
-    end
+fdescribe HomesEngland::Gateway::Pcs do
+  context 'existing bid' do
+    context 'Example 1' do
+      let(:pcs_secret) { '1119003331' }
+      let(:auth_token) { JWT.encode({}, pcs_secret, 'HS512') }
+      let(:pcs_domain) { 'https://meow.cat' }
+      let(:pcs_overview_request) do
+        stub_request(
+          :get,
+          "#{pcs_domain}/pcs-api/v1/Projects/HIF%252FMV%252F255"
+        ).to_return(
+          status: 200,
+          body: {
+            ProjectManager: "Ed",
+            Sponsor: "FIS"
+          }.to_json
+        ).with(
+          headers: {'Authorization' => "Bearer #{auth_token}" }
+        )
+      end
 
-    let(:pcs_actuals_request) do
-      stub_request(
-        :get,
-        "#{pcs_domain}/pcs-api/v1/Projects/HIF%252FMV%252F255/actuals"
-      ).to_return(
-        status: 200,
-        body: [
+      let(:pcs_actuals_request) do
+        stub_request(
+          :get,
+          "#{pcs_domain}/pcs-api/v1/Projects/HIF%252FMV%252F255/actuals"
+        ).to_return(
+          status: 200,
+          body: [
+            {
+              dateInfo: {
+                period: '2018/19'
+              },
+              payments: {
+                currentYearPayments:
+                [111900, 25565, 14159265]
+              }
+            }
+          ].to_json
+        ).with(
+          headers: {'Authorization' => "Bearer #{auth_token}" }
+        )
+      end
+
+      let(:gateway) { described_class.new }
+
+      before do
+        ENV['PCS_DOMAIN'] = pcs_domain
+        ENV['PCS_SECRET'] = pcs_secret
+        pcs_overview_request
+        pcs_actuals_request
+        gateway
+      end
+
+      it 'Calls the PCS overview endpoint' do
+        gateway.get_project(bid_id: 'HIF/MV/255')
+        expect(pcs_overview_request).to have_been_requested
+      end
+
+      it 'Calls the PCS actuals endpoint' do
+        gateway.get_project(bid_id: 'HIF/MV/255')
+        expect(pcs_actuals_request).to have_been_requested
+      end
+
+      it 'Returns a domain object' do
+        project = gateway.get_project(bid_id: 'HIF/MV/255')
+
+        expect(project.project_manager).to eq("Ed")
+        expect(project.sponsor).to eq("FIS")
+        expect(project.actuals).to eq([
           {
             dateInfo: {
               period: '2018/19'
@@ -34,77 +76,77 @@ describe HomesEngland::Gateway::Pcs do
               [111900, 25565, 14159265]
             }
           }
-        ].to_json
-      ).with(
-        headers: {'Authorization' => "Bearer #{auth_token}" }
-      )
+        ])
+      end
     end
 
-    let(:gateway) { described_class.new }
+    context 'Example 2' do
+      let(:pcs_secret) { '0118999611993' }
+      let(:auth_token) { JWT.encode({}, pcs_secret, 'HS512') }
+      let(:pcs_domain) { 'http://simulator' }
 
-    before do
-      ENV['PCS_DOMAIN'] = pcs_domain
-      ENV['PCS_SECRET'] = pcs_secret
-      pcs_overview_request
-      pcs_actuals_request
-      gateway
-    end
+      let(:pcs_overview_request) do
+        stub_request(
+          :get,
+          "#{pcs_domain}/pcs-api/v1/Projects/AC%252FMV%252F151"
+        ).to_return(
+            status: 200, body: {
+            ProjectManager: "Natalia",
+            Sponsor: "NHN"
+          }.to_json
+        ).with(
+          headers: { 'Authorization' => "Bearer #{auth_token}" }
+        )
+      end
 
-    it 'Calls the PCS overview endpoint' do
-      gateway.get_project(bid_id: 'HIF/MV/255')
-      expect(pcs_overview_request).to have_been_requested
-    end
+      let(:pcs_actuals_request) do
+        stub_request(
+          :get,
+          "#{pcs_domain}/pcs-api/v1/Projects/AC%252FMV%252F151/actuals"
+        ).to_return(
+          status: 200,
+          body: [
+            {
+              dateInfo: {
+                period: '2007/2008'
+              },
+              payments: {
+                currentYearPayments:
+                [800999, 41199, 1989, 2012]
+              }
+            }
+          ].to_json
+        ).with(
+          headers: {'Authorization' => "Bearer #{auth_token}" }
+        )
+      end
 
-    it 'Calls the PCS actuals endpoint' do
-      gateway.get_project(bid_id: 'HIF/MV/255')
-      expect(pcs_actuals_request).to have_been_requested
-    end
+      let(:gateway) { described_class.new }
 
-    it 'Returns a domain object' do
-      project = gateway.get_project(bid_id: 'HIF/MV/255')
+      before do
+        ENV['PCS_DOMAIN'] = pcs_domain
+        ENV['PCS_SECRET'] = pcs_secret
+        pcs_overview_request
+        pcs_actuals_request
+        gateway
+      end
 
-      expect(project.project_manager).to eq("Ed")
-      expect(project.sponsor).to eq("FIS")
-      expect(project.actuals).to eq([
-        {
-          dateInfo: {
-            period: '2018/19'
-          },
-          payments: {
-            currentYearPayments:
-            [111900, 25565, 14159265]
-          }
-        }
-      ])
-    end
-  end
+      it 'Calls the PCS overview endpoint' do
+        gateway.get_project(bid_id: 'AC/MV/151')
+        expect(pcs_overview_request).to have_been_requested
+      end
 
-  context 'Example 2' do
-    let(:pcs_secret) { '0118999611993' }
-    let(:auth_token) { JWT.encode({}, pcs_secret, 'HS512') }
-    let(:pcs_domain) { 'http://simulator' }
+      it 'Calls the PCS actuals endpoint' do
+        gateway.get_project(bid_id: 'AC/MV/151')
+        expect(pcs_actuals_request).to have_been_requested
+      end
 
-    let(:pcs_overview_request) do
-      stub_request(
-        :get,
-        "#{pcs_domain}/pcs-api/v1/Projects/AC%252FMV%252F151"
-      ).to_return(
-          status: 200, body: {
-          ProjectManager: "Natalia",
-          Sponsor: "NHN"
-        }.to_json
-      ).with(
-        headers: { 'Authorization' => "Bearer #{auth_token}" }
-      )
-    end
+      it 'Returns a domain object' do
+        project = gateway.get_project(bid_id: 'AC/MV/151')
 
-    let(:pcs_actuals_request) do
-      stub_request(
-        :get,
-        "#{pcs_domain}/pcs-api/v1/Projects/AC%252FMV%252F151/actuals"
-      ).to_return(
-        status: 200,
-        body: [
+        expect(project.project_manager).to eq("Natalia")
+        expect(project.sponsor).to eq("NHN")
+        expect(project.actuals).to eq([
           {
             dateInfo: {
               period: '2007/2008'
@@ -114,48 +156,104 @@ describe HomesEngland::Gateway::Pcs do
               [800999, 41199, 1989, 2012]
             }
           }
-        ].to_json
-      ).with(
-        headers: {'Authorization' => "Bearer #{auth_token}" }
-      )
+        ])
+      end
+    end
+  end
+
+  context 'non-existent bid' do
+    context 'example 1' do
+      let(:pcs_secret) { '1119003331' }
+      let(:auth_token) { JWT.encode({}, pcs_secret, 'HS512') }
+      let(:pcs_domain) { 'https://meow.cat' }
+      let(:pcs_overview_request) do
+        stub_request(
+          :get,
+          "#{pcs_domain}/pcs-api/v1/Projects/HIF%252FMV%252F255"
+        ).to_return(
+          status: 404,
+          body: {
+            ProjectManager: "Ed",
+            Sponsor: "FIS"
+          }.to_json
+        ).with(
+          headers: {'Authorization' => "Bearer #{auth_token}" }
+        )
+      end
+
+      let(:pcs_actuals_request) do
+        stub_request(
+          :get,
+          "#{pcs_domain}/pcs-api/v1/Projects/HIF%252FMV%252F255/actuals"
+        ).to_return(
+          status: 404
+        ).with(
+          headers: {'Authorization' => "Bearer #{auth_token}" }
+        )
+      end
+
+      let(:gateway) { described_class.new }
+
+      before do
+        ENV['PCS_DOMAIN'] = pcs_domain
+        ENV['PCS_SECRET'] = pcs_secret
+        pcs_overview_request
+        pcs_actuals_request
+        gateway
+      end
+      
+      it 'Returns nil' do
+        project = gateway.get_project(bid_id: 'HIF/MV/255')
+
+        expect(project).to eq(nil)
+      end
     end
 
-    let(:gateway) { described_class.new }
+    context 'example 2' do
+      let(:pcs_secret) { '1119003331' }
+      let(:auth_token) { JWT.encode({}, pcs_secret, 'HS512') }
+      let(:pcs_domain) { 'https://meow.cat' }
+      let(:pcs_overview_request) do
+        stub_request(
+          :get,
+          "#{pcs_domain}/pcs-api/v1/Projects/AC%252FMV%252F1"
+        ).to_return(
+          status: 404,
+          body: {
+            ProjectManager: "Ed",
+            Sponsor: "FIS"
+          }.to_json
+        ).with(
+          headers: {'Authorization' => "Bearer #{auth_token}" }
+        )
+      end
 
-    before do
-      ENV['PCS_DOMAIN'] = pcs_domain
-      ENV['PCS_SECRET'] = pcs_secret
-      pcs_overview_request
-      pcs_actuals_request
-      gateway
-    end
+      let(:pcs_actuals_request) do
+        stub_request(
+          :get,
+          "#{pcs_domain}/pcs-api/v1/Projects/AC%252FMV%252F1/actuals"
+        ).to_return(
+          status: 404
+        ).with(
+          headers: {'Authorization' => "Bearer #{auth_token}" }
+        )
+      end
 
-    it 'Calls the PCS overview endpoint' do
-      gateway.get_project(bid_id: 'AC/MV/151')
-      expect(pcs_overview_request).to have_been_requested
-    end
+      let(:gateway) { described_class.new }
 
-    it 'Calls the PCS actuals endpoint' do
-      gateway.get_project(bid_id: 'AC/MV/151')
-      expect(pcs_actuals_request).to have_been_requested
-    end
+      before do
+        ENV['PCS_DOMAIN'] = pcs_domain
+        ENV['PCS_SECRET'] = pcs_secret
+        pcs_overview_request
+        pcs_actuals_request
+        gateway
+      end
 
-    it 'Returns a domain object' do
-      project = gateway.get_project(bid_id: 'AC/MV/151')
+      it 'Returns nil' do
+        project = gateway.get_project(bid_id: 'AC/MV/1')
 
-      expect(project.project_manager).to eq("Natalia")
-      expect(project.sponsor).to eq("NHN")
-      expect(project.actuals).to eq([
-        {
-          dateInfo: {
-            period: '2007/2008'
-          },
-          payments: {
-            currentYearPayments:
-            [800999, 41199, 1989, 2012]
-          }
-        }
-      ])
+        expect(project).to eq(nil)
+      end
     end
   end
 end
