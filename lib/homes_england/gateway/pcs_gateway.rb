21058a1fd0f9b32c0d6cc5fe834704c3df55d466
Faissal Bensefia <faissal@madetech.com>
2019-03-07 11:27:45 +0000

    Refactor

--- lib/homes_england/gateway/pcs_gateway.rb
+++ lib/homes_england/gateway/pcs_gateway.rb
@@ -19,30 +19,31 @@ class HomesEngland::Gateway::Pcs
   end
 
   def request_pcs_data(bid_id, endpoint)
-    pcs_key = JWT.encode({
-      exp: Time.now.to_i + (60 * 60 * 24 * 30)
-      }, ENV['PCS_SECRET'], 'HS512')
-    host = URI.parse(@pcs_domain).host
-    protocol = URI.parse(@pcs_domain).scheme
-
-    if protocol == 'http'
-      port = Net::HTTP.http_default_port()
-      pcs_endpoint = Net::HTTP.new(host, port)
-    else
-      port = Net::HTTP.https_default_port()
-      pcs_endpoint = Net::HTTP.new(host, port)
-      pcs_endpoint.use_ssl = true
-    end
-
-    request = Net::HTTP::Get.new("/pcs-api/v1/Projects/#{encode_bid_id(bid_id)}#{endpoint}")
-    request['Authorization'] = "Bearer #{pcs_key}"
-    response = pcs_endpoint.request(request)
-    if response.kind_of?(Net::HTTPSuccess)
-      Common::DeepSymbolizeKeys.to_symbolized_hash(JSON.parse(response.body))
+    pcs_domain_uri = URI(@pcs_domain)
+    use_ssl = pcs_domain_uri.scheme == 'https'
+    Net::HTTP.start(pcs_domain_uri.host, pcs_domain_uri.port, use_ssl: use_ssl) do |http|
+      request = Net::HTTP::Get.new("/pcs-api/v1/Projects/#{encode_bid_id(bid_id)}#{endpoint}")
+      request['Authorization'] = "Bearer #{generate_pcs_key()}"
+      response = http.request(request)
+      if response.kind_of?(Net::HTTPSuccess)
+        Common::DeepSymbolizeKeys.to_symbolized_hash(JSON.parse(response.body))
+      end
     end
   end
 
   def encode_bid_id(bid_id)
     ERB::Util.url_encode(ERB::Util.url_encode(bid_id))
+  end
+
+  def generate_pcs_key()
+    thirty_days_in_seconds = 60 * 60 * 24 * 30
+    thirty_days_from_now = Time.now.to_i + thirty_days_in_seconds
+    JWT.encode(
+      {
+        exp: thirty_days_from_now
+      },
+      ENV['PCS_SECRET'],
+      'HS512'
+    )
   end
 end
