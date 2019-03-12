df74bf239df0adc818d02b563336e9264b352a95
Faissal Bensefia <faissal@madetech.com>
2019-03-12 13:44:31 +0000

    Remove PCS data for claims from returns

--- lib/local_authority/use_case/pcs_populate_return.rb
+++ lib/local_authority/use_case/pcs_populate_return.rb
@@ -11,16 +11,6 @@ class LocalAuthority::UseCase::PcsPopulateReturn
       pcs_data = @pcs_gateway.get_project(bid_id: return_data[:bid_id])
 
       unless pcs_data.nil? || pcs_data.actuals.nil?
-        total = get_spend_to_date(pcs_data)
-        if return_data[:type] == 'ac'
-          return_data[:updates][-1][:s151GrantClaimApproval] = {} if return_data[:s151GrantClaimApproval].nil?
-          return_data[:updates][-1][:s151GrantClaimApproval][:SpendToDate] = total.to_s
-        elsif return_data[:type] == 'hif'
-          return_data[:updates][-1][:s151] = {} if return_data[:updates][-1][:s151].nil?
-          return_data[:updates][-1][:s151][:claimSummary] = {} if return_data.dig(:updates, -1, :s151, :claimSummary).nil?
-          return_data[:updates][-1][:s151][:claimSummary][:hifSpendToDate] = total.to_s
-        end
-
         return_data[:updates][-1] = add_ac_figures(return_data[:updates][-1], pcs_data.actuals) if return_data[:type] == 'ac'
       end
     end
@@ -28,12 +18,6 @@ class LocalAuthority::UseCase::PcsPopulateReturn
   end
 
   private
-
-  def get_spend_to_date(pcs_data)
-    pcs_data.actuals.reduce(0) do |sum, actual|
-      sum + actual.dig(:payments,:currentYearPayments).sum + actual[:previousYearPaymentsToDate].to_i
-    end
-  end
 
   def add_ac_figures(return_data, actuals)
     year = actuals[0][:dateInfo][:period]
