5ab954951d4ac8a19ae223530b3628aa87b995ea
Faissal Bensefia <faissal@madetech.com>
2019-03-12 11:34:02 +0000

    Add PCS data to claims

--- lib/local_authority/use_case/pcs_populate_claim.rb
+++ lib/local_authority/use_case/pcs_populate_claim.rb
@@ -7,9 +7,23 @@ class LocalAuthority::UseCase::PcsPopulateClaim
   def execute(claim_id:)
     found_claim = @get_claim_core.execute(claim_id: claim_id)
     unless ENV['PCS'].nil?
-      @pcs_gateway.get_project(bid_id: found_claim[:bid_id])
+      pcs_data = @pcs_gateway.get_project(bid_id: found_claim[:bid_id])
+      total = get_spend_to_date(pcs_data)
+      if found_claim[:type] == 'hif'
+        found_claim[:data][:claimSummary] = {} if found_claim.dig(:data, :claimSummary).nil?
+        found_claim[:data][:claimSummary][:hifSpendToDate] = total.to_s
+      else
+        found_claim[:data][:SpendToDate] = total.to_s
+      end
+
     end
 
     found_claim
+  end
+
+  def get_spend_to_date(pcs_data)
+    pcs_data.actuals.reduce(0) do |sum, actual|
+      sum + actual.dig(:payments,:currentYearPayments).sum + actual[:previousYearPaymentsToDate].to_i
+    end
   end
 end
--- spec/unit/local_authority/use_case/pcs_populate_claim_spec.rb
+++ spec/unit/local_authority/use_case/pcs_populate_claim_spec.rb
@@ -1,4 +1,4 @@
-describe LocalAuthority::UseCase::PcsPopulateClaim do
+fdescribe LocalAuthority::UseCase::PcsPopulateClaim do
   let(:pcs_gateway_spy) { nil }
 
   let(:usecase) do
@@ -79,75 +79,289 @@ describe LocalAuthority::UseCase::PcsPopulateClaim do
 
 
     let(:pcs_gateway_spy) do
-      spy(
-        get_project:
-        HomesEngland::Domain::PcsBid.new.tap do |project|
-          project.project_manager = "Thomas Jenkins"
-          project.sponsor = "Toh Kay"
-          project.actuals = [
-            {
-              dateInfo: {
-                period: '2018/19',
-                monthNumber: 12
-              },
-              previousYearPaymentsToDate: 12,
-              payments: {
-                currentYearPayments: [
-                  1,
-                  2,
-                  4,
-                  8,
-                  16,
-                  32,
-                  64,
-                  128,
-                  256,
-                  512,
-                  1024,
-                  2048
-                ]
+      spy(get_project: pcs_bid)
+    end
+
+
+    context 'hif' do
+      context 'example 1' do
+        let(:pcs_bid) do
+          HomesEngland::Domain::PcsBid.new.tap do |project|
+            project.project_manager = "Thomas Jenkins"
+            project.sponsor = "Toh Kay"
+            project.actuals = [
+              {
+                dateInfo: {
+                  period: '2018/19',
+                  monthNumber: 12
+                },
+                previousYearPaymentsToDate: 12,
+                payments: {
+                  currentYearPayments: [
+                    1,
+                    2,
+                    4,
+                    8,
+                    16,
+                    32,
+                    64,
+                    128,
+                    256,
+                    512,
+                    1024,
+                    2048
+                  ]
+                }
+              }
+            ]
+          end
+        end
+
+        let(:get_claim_core_spy) do
+          spy(
+            execute: {
+              id: 1,
+              bid_id: 'HIF/MV/151',
+              type: 'hif',
+              project_id: 2,
+              status: 'Draft',
+              data: {
+                claimSummary: {
+                  otherData: 'value'
+                },
+                claimKey: 'claimValue'
               }
             }
-          ]
+          )
         end
-      )
-    end
 
-    context 'example 1' do
-      let(:get_claim_core_spy) do
-        spy(
-          execute: {
+        it 'calls the pcs gateway with the bid id' do
+          usecase.execute(claim_id: 1)
+          expect(pcs_gateway_spy).to have_received(:get_project).with(bid_id: 'HIF/MV/151')
+        end
+
+        it 'returns the appropriate data' do
+          expect(usecase.execute(claim_id: 1)).to eq({
+            id: 1,
             bid_id: 'HIF/MV/151',
             type: 'hif',
+            project_id: 2,
+            status: 'Draft',
             data: {
-              claimKey: "claimValue"
+              claimSummary: {
+                hifSpendToDate: '4107',
+                otherData: 'value'
+              },
+              claimKey: 'claimValue'
             }
-          }
-        )
+          })
+        end
       end
 
-      it 'calls the pcs gateway with the bid id' do
-        usecase.execute(claim_id: 1)
-        expect(pcs_gateway_spy).to have_received(:get_project).with(bid_id: 'HIF/MV/151')
+      context 'example 2' do
+        let(:pcs_bid) do
+          HomesEngland::Domain::PcsBid.new.tap do |project|
+            project.project_manager = "Thomas Jenkins"
+            project.sponsor = "Toh Kay"
+            project.actuals = [
+              {
+                dateInfo: {
+                  period: '2018/19',
+                  monthNumber: 12
+                },
+                previousYearPaymentsToDate: 3,
+                payments: {
+                  currentYearPayments: [
+                    1,
+                    4,
+                    1,
+                    5,
+                    9,
+                    2,
+                    6,
+                    5,
+                    3,
+                    5,
+                    8,
+                    9
+                  ]
+                }
+              }
+            ]
+          end
+        end
+
+        let(:get_claim_core_spy) do
+          spy(
+            execute: {
+              id: 3,
+              bid_id: 'HIF/MV/11',
+              type: 'hif',
+              status: 'Draft',
+              project_id: 4,
+              data: {
+                claimKey: "claimValue"
+              }
+            }
+          )
+        end
+
+        it 'calls the pcs gateway with the bid id' do
+          usecase.execute(claim_id: 3)
+          expect(pcs_gateway_spy).to have_received(:get_project).with(bid_id: 'HIF/MV/11')
+        end
+
+        it 'returns the appropriate data' do
+          expect(usecase.execute(claim_id: 1)).to eq({
+            id: 3,
+            bid_id: 'HIF/MV/11',
+            type: 'hif',
+            project_id: 4,
+            status: 'Draft',
+            data: {
+              claimSummary: {hifSpendToDate: '61'},
+              claimKey: "claimValue"
+            }
+          })
+        end
       end
     end
 
-    context 'example 2' do
-      let(:get_claim_core_spy) do
-        spy(
-          execute: {
-            bid_id: 'AC/MV/11',
-            type: 'hif',
-            data: {
-              claimKey: "claimValue"
+    context 'ac' do
+      context 'example 1' do
+        let(:pcs_bid) do
+          HomesEngland::Domain::PcsBid.new.tap do |project|
+            project.project_manager = "Thomas Jenkins"
+            project.sponsor = "Toh Kay"
+            project.actuals = [
+              {
+                dateInfo: {
+                  period: '2018/19',
+                  monthNumber: 12
+                },
+                previousYearPaymentsToDate: 12,
+                payments: {
+                  currentYearPayments: [
+                    1,
+                    2,
+                    4,
+                    8,
+                    16,
+                    32,
+                    64,
+                    128,
+                    256,
+                    512,
+                    1024,
+                    2048
+                  ]
+                }
+              }
+            ]
+          end
+        end
+
+        let(:get_claim_core_spy) do
+          spy(
+            execute: {
+              id: 1,
+              bid_id: 'AC/MV/151',
+              type: 'ac',
+              project_id: 2,
+              status: 'Draft',
+              data: {
+                claimKey: 'claimValue',
+              }
             }
-          }
-        )
+          )
+        end
+
+        it 'calls the pcs gateway with the bid id' do
+          usecase.execute(claim_id: 1)
+          expect(pcs_gateway_spy).to have_received(:get_project).with(bid_id: 'AC/MV/151')
+        end
+
+        it 'returns the appropriate data' do
+          expect(usecase.execute(claim_id: 1)).to eq({
+            id: 1,
+            bid_id: 'AC/MV/151',
+            type: 'ac',
+            project_id: 2,
+            status: 'Draft',
+            data: {
+              SpendToDate: '4107',
+              claimKey: 'claimValue'
+            }
+          })
+        end
       end
 
-      it 'calls the pcs gateway with the bid id' do
-        usecase.execute(claim_id: 11)
-        expect(pcs_gateway_spy).to have_received(:get_project).with(bid_id: 'AC/MV/11')
+      context 'example 2' do
+        let(:pcs_bid) do
+          HomesEngland::Domain::PcsBid.new.tap do |project|
+            project.project_manager = "Thomas Jenkins"
+            project.sponsor = "Toh Kay"
+            project.actuals = [
+              {
+                dateInfo: {
+                  period: '2018/19',
+                  monthNumber: 12
+                },
+                previousYearPaymentsToDate: 3,
+                payments: {
+                  currentYearPayments: [
+                    1,
+                    4,
+                    1,
+                    5,
+                    9,
+                    2,
+                    6,
+                    5,
+                    3,
+                    5,
+                    8,
+                    9
+                  ]
+                }
+              }
+            ]
+          end
+        end
+
+        let(:get_claim_core_spy) do
+          spy(
+            execute: {
+              id: 3,
+              bid_id: 'AC/MV/11',
+              type: 'ac',
+              status: 'Draft',
+              project_id: 4,
+              data: {
+                claimKey: "claimValue"
+              }
+            }
+          )
+        end
+
+        it 'calls the pcs gateway with the bid id' do
+          usecase.execute(claim_id: 3)
+          expect(pcs_gateway_spy).to have_received(:get_project).with(bid_id: 'AC/MV/11')
+        end
+
+        it 'returns the appropriate data' do
+          expect(usecase.execute(claim_id: 1)).to eq({
+            id: 3,
+            bid_id: 'AC/MV/11',
+            type: 'ac',
+            project_id: 4,
+            status: 'Draft',
+            data: {
+              SpendToDate: '61',
+              claimKey: "claimValue"
+            }
+          })
+        end
       end
     end
   end
