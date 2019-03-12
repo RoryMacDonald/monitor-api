5319c8d9ffff1e2d3c5c33f438f301c928360f54
Christopher Parsons <mail@christopherparsons.me>
2019-03-12 09:56:44 +0000

    Add tests around environment variable for ff project creation

--- spec/unit/ui/gateway/in_memory_new_project_spec.rb
+++ spec/unit/ui/gateway/in_memory_new_project_spec.rb
@@ -1,19 +1,42 @@
 describe UI::Gateway::InMemoryNewProject do
-  it 'Returns a blank project when finding by type hif' do
-    gateway = described_class.new
-    project = gateway.find_by(type: 'hif')
-    expect(project).not_to be_nil
+  context 'with environment variables enabled' do
+    before do
+      ENV['FF_CREATION'] = 'yes'
+    end
+    after do
+      ENV['FF_CREATION'] = nil
+    end
+
+    it 'Returns a blank project when finding by type hif' do
+      gateway = described_class.new
+      project = gateway.find_by(type: 'hif')
+      expect(project).not_to be_nil
+    end
+
+    it 'Returns a blank project when finding by type ac' do
+      gateway = described_class.new
+      project = gateway.find_by(type: 'ac')
+      expect(project).not_to be_nil
+    end
+
+    it 'Returns a blank project when finding by type ff' do
+      gateway = described_class.new
+      project = gateway.find_by(type: 'ff')
+      expect(project).not_to be_nil
+    end
+
+    it 'Returns nil when searching for a non existing type' do
+      gateway = described_class.new
+      schema = gateway.find_by(type: 'cats 4 lyf')
+      expect(schema).to be_nil
+    end
   end
 
-  it 'Returns a blank project when finding by type ac' do
-    gateway = described_class.new
-    project = gateway.find_by(type: 'ac')
-    expect(project).not_to be_nil
-  end
-
-  it 'Returns nil when searching for a non existing type' do
-    gateway = described_class.new
-    schema = gateway.find_by(type: 'cats 4 lyf')
-    expect(schema).to be_nil
+  context 'without environment variables enabled' do
+    it 'Does not return a blank project for ff' do
+      gateway = described_class.new
+      project = gateway.find_by(type: 'ff')
+      expect(project).to be_nil
+    end
   end
 end
