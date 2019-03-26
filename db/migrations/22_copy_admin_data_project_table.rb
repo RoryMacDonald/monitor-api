d6d1b8d80302b1d031a064b2ae2fd286120670be
Emma Corbett <emma@madetech.com>
2019-03-26 16:49:45 +0000

    Migrate hif admin data to the project table

---
+++ db/migrations/22_copy_admin_data_project_table.rb
@@ -0,0 +1,26 @@
+Sequel.migration do
+  up do
+    alter_table(:projects) do
+      add_column :data, 'json'
+    end
+    
+    projects = from(:projects)
+
+    projects.each do |project|
+      next if !project.nil? && project[:type] != 'hif'
+      baselines = from(:baselines).where(project_id: project[:id]).order.all
+
+      baselines = baselines.select { |base| base[:status] == 'Submitted' } if baselines.length > 1
+
+      last_baseline = baselines.last
+
+      from(:projects).where(id: project[:id]).update(data: Sequel.pg_json(last_baseline[:data]['rmBaseline']))
+    end
+  end
+
+  down do
+    alter_table(:projects) do
+      drop_column :data
+    end
+  end
+end
