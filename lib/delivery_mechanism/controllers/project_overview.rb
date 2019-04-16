79cb0235fa40fcecbf311b422c67e6eca88fd530
faissalMT <41294831+faissalMT@users.noreply.github.com>
2019-04-16 16:49:04 +0100

    Update lib/delivery_mechanism/controllers/project_overview.rb
    Co-Authored-By: danielburnley <dan@danops.uk>

--- lib/delivery_mechanism/controllers/project_overview.rb
+++ lib/delivery_mechanism/controllers/project_overview.rb
@@ -1,7 +1,7 @@
 require_relative '../web_routes.rb'
 
 DeliveryMechanism::WebRoutes.get '/project/:id/overview' do
-  guard_access env, params, request do |_|
+  guard_access env, params, request do
     project_overview = @dependency_factory.get_use_case(:get_project_overview).execute(
       id: params[:id].to_i
     )
