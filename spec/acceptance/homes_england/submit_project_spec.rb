7dfd0db02bb0a0332be0292c941d344d5d917752
Daniel Burnley <dan@danops.uk>
2019-05-07 15:31:15 +0100

    Update submit project spec
    Co-authored-by: Wen Ting Wang <chubberlisk@outlook.com>

--- spec/acceptance/homes_england/submit_project_spec.rb
+++ spec/acceptance/homes_england/submit_project_spec.rb
@@ -2,41 +2,37 @@
 
 require 'rspec'
 require_relative '../shared_context/dependency_factory'
+require_relative '../shared_context/project_fixtures'
 
 describe 'Submitting a completed draft project' do
   include_context 'dependency factory'
+  include_context 'project fixtures'
 
-  context 'Whilst status is Draft' do
-    it 'Changes the status to submitted' do
-      project_baseline = {
-        summary: {
-          project_name: '',
-          description: '',
-          lead_authority: ''
-        },
-        infrastructure: {
-          type: '',
-          description: '',
-          completion_date: '',
-          planning: {
-            submission_estimated: ''
-          }
-        },
-        financial: {
-          total_amount_estimated: ''
-        }
-      }
-      response = get_use_case(:create_new_project).execute(
+  context 'Submitting a draft project' do
+    let(:project_id) do
+      get_use_case(:create_new_project).execute(
         name: 'cat project',
         type: 'hif',
-        baseline: project_baseline,
+        baseline: hif_mvf_core_project_baseline,
         bid_id: 'HIF/MV/151'
-      )
+      )[:id]
+    end
 
-      get_use_case(:submit_project).execute(project_id: response[:id])
+    let(:submitted_project) { get_use_case(:find_project).execute(id: project_id) }
 
-      submitted_project = get_use_case(:find_project).execute(id: response[:id])
+    def given_a_draft_project
+      project_id
+      get_use_case(:submit_project).execute(project_id: project_id)
+      submitted_project
+    end
+
+    def then_the_project_is_submitted
       expect(submitted_project[:status]).to eq('Submitted')
+    end
+
+    it 'Changes the status to submitted' do
+      given_a_draft_project
+      then_the_project_is_submitted
     end
   end
 end
