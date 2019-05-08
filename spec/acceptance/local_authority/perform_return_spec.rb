609bd44d9efbe99d022cd1fa52f20e7c26a4cc58
Wen Ting Wang <chubberlisk@outlook.com>
2019-05-08 12:26:22 +0100

    Refactor perform return acceptance test
    Co-authored-by: Robert McHugh <robert@madetech.com>

--- spec/acceptance/local_authority/perform_return_spec.rb
+++ spec/acceptance/local_authority/perform_return_spec.rb
@@ -9,55 +9,8 @@ describe 'Performing Return on HIF Project' do
   let(:pcs_domain) { 'meow.cat' }
   let(:api_key) { 'C.B.R' }
 
-  def get_return(id:)
-    stub_request(:get, "http://#{pcs_domain}/project/#{id}").to_return(
-      status: 200,
-      body: {
-        ProjectManager: "Michael",
-        Sponsor: "MSPC"
-      }.to_json
-    ).with(
-      headers: {'Authorization' => "Bearer #{api_key}" }
-    )
-
-    get_use_case(:get_return).execute(id: id)
-  end
-
-  def update_return(id:, data:)
-    get_use_case(:update_return).execute(return_id: id, data: data[:data])
-  end
-
-  def submit_return(id:)
-    get_use_case(:submit_return).execute(return_id: id)
-  end
-
-  def create_new_return(return_data)
-    get_use_case(:create_return).execute(return_data)[:id]
-  end
-
-  def expect_return_with_id_to_equal(id:, expected_return:)
-    found_return = get_return(id: id)
-    expect(found_return[:data]).to eq(expected_return[:data])
-    expect(found_return[:status]).to eq(expected_return[:status])
-    expect(found_return[:updates]).to eq(expected_return[:updates])
-  end
-
-  def expect_return_to_be_submitted(id:)
-    found_return = get_return(id: id)
-    expect(found_return[:status]).to eq('Submitted')
-  end
-
   let(:project_baseline) do
     File.open("#{__dir__}/../../fixtures/hif_baseline_core.json") do |f|
-      JSON.parse(
-        f.read,
-        symbolize_names: true
-      )
-    end
-  end
-
-  let(:ac_project_baseline) do
-    File.open("#{__dir__}/../../fixtures/ac_baseline_core.json") do |f|
       JSON.parse(
         f.read,
         symbolize_names: true
@@ -71,15 +24,6 @@ describe 'Performing Return on HIF Project' do
       type: 'hif',
       baseline: project_baseline,
       bid_id: 'HIF/MV/16'
-    )[:id]
-  end
-
-  let(:ac_project_id) do
-    get_use_case(:create_new_project).execute(
-      name: '',
-      type: 'ac',
-      baseline: ac_project_baseline,
-      bid_id: 'AC/MV/6'
     )[:id]
   end
 
@@ -110,80 +54,29 @@ describe 'Performing Return on HIF Project' do
     end
   end
 
-  before do
-    ENV['PCS'] = 'yes'
-    ENV['PCS_DOMAIN'] = pcs_domain
-    ENV['PCS_SECRET'] = 'Secret 2'
-    ENV['OUTPUTS_FORECAST_TAB'] = 'Yes'
-    ENV['CONFIRMATION_TAB'] = 'Yes'
-    ENV['S151_TAB'] = 'Yes'
-    ENV['RM_MONTHLY_CATCHUP_TAB'] = 'Yes'
-    ENV['MR_REVIEW_TAB'] = 'Yes'
-    ENV['OUTPUTS_ACTUALS_TAB'] = 'Yes'
-    ENV['HIF_RECOVERY_TAB'] = 'Yes'
-    project_id
-  end
-
-  after do
-    ENV['OUTPUTS_FORECAST_TAB'] = nil
-    ENV['CONFIRMATION_TAB'] = nil
-    ENV['PCS_SECRET'] = nil
-    ENV['S151_TAB'] = nil
-    ENV['MR_REVIEW_TAB'] = nil
-    ENV['OUTPUTS_ACTUALS_TAB'] = nil
-    ENV['HIF_RECOVERY_TAB'] = nil
-    ENV['PCS'] = nil
-    ENV['PCS_DOMAIN'] = nil
-  end
-
-  it 'should keep track of Returns' do
-    base_return = get_use_case(:get_base_return).execute(project_id: project_id)
-
-    expect(base_return[:base_return][:data]).to eq(expected_base_return)
-
-    initial_return = {
+  let(:initial_return) do
+    {
       project_id: project_id,
-      data: {
-        summary: {
-          project_name: 'Cats Protection League',
-          description: 'A new headquarters for all the Cats',
-          lead_authority: 'Made Tech'
-        },
-        infrastructures: [
-          {
-            type: 'Cat Bathroom',
-            description: 'Bathroom for Cats',
-            completion_date: '2018-12-25',
-            planning: {
-              submission_estimated: '2018-06-01',
-              submission_actual: '2018-07-01',
-              submission_delay_reason: 'Planning office was closed for summer',
-              planningNotGranted: {
-                fieldOne: {
-                  varianceCalculations: {
-                    varianceAgainstLastReturn: {
-                      varianceLastReturnFullPlanningPermissionSubmitted: nil
-                    }
-                  }
-                }
-              }
-            }
-          }
-        ],
-        financial: {
-          total_amount_estimated: '£ 1000000.00',
-          total_amount_actual: nil,
-          total_amount_changed_reason: nil
-        }
-      }
+      data: File.open("#{__dir__}/../../fixtures/hif_return_core.json") do |f|
+              JSON.parse(
+                f.read,
+                symbolize_names: true
+              )
+            end
     }
+  end
 
-    return_id = create_new_return(
+  let(:base_return) { get_use_case(:get_base_return).execute(project_id: project_id) }
+
+  let(:return_id) do
+    create_new_return(
       project_id: initial_return[:project_id],
       data: initial_return[:data]
     )
+  end
 
-    expected_initial_return = {
+  let(:expected_initial_return) do
+    {
       baseline_version: 1,
       project_id: project_id,
       status: 'Draft',
@@ -191,17 +84,14 @@ describe 'Performing Return on HIF Project' do
         initial_return[:data]
       ]
     }
+  end
 
-    expect_return_with_id_to_equal(
-      id: return_id,
-      expected_return: expected_initial_return
-    )
-
-    updated_return_data = {
+  let(:updated_return_data) do
+    {
       s151: {
         supportingEvidence: {
           lastQuarterMonthSpend: {
-            forecast: "1"
+            forecast: '1'
           }
         }
       },
@@ -303,7 +193,7 @@ describe 'Performing Return on HIF Project' do
           milestones: {
             keyMilestones: [
               {
-                currentReturn: "12/12/2012",
+                currentReturn: '12/12/2012',
                 statusAgainstLastReturn: 'Complete',
                 milestoneCompletedDate: '1'
               }
@@ -367,19 +257,18 @@ describe 'Performing Return on HIF Project' do
         recovery: {
           expectedAmountToRecover: {
             changeToBaseline: {
-              confirmation: "Yes",
-              lastReturn: "12",
-              currentCopy: "14"
+              confirmation: 'Yes',
+              lastReturn: '12',
+              currentCopy: '14'
             }
           }
         }
       }
     }
+  end
 
-    soft_update_return(id: return_id, data: updated_return_data)
-    soft_update_return(id: return_id, data: updated_return_data)
-
-    expected_updated_return = {
+  let(:expected_updated_return) do
+    {
       baseline_version: 1,
       project_id: project_id,
       status: 'Draft',
@@ -389,40 +278,38 @@ describe 'Performing Return on HIF Project' do
         updated_return_data
       ]
     }
-
-    expect_return_with_id_to_equal(
-      id: return_id,
-      expected_return: expected_updated_return
-    )
-
-    submit_return(id: return_id)
-    expect_return_to_be_submitted(id: return_id)
-
-    # A draft
-    create_new_return(
-      project_id: project_id,
-      data: initial_return[:data]
-    )
-
-    second_base_return = get_use_case(:get_base_return).execute(project_id: project_id)
-
-    expect(second_base_return[:base_return][:data][:infrastructures]).to eq(expected_second_base_return[:infrastructures])
-
-    return_data = get_use_case(:get_return).execute(id: return_id)
-    expect(return_data[:baseline_version]).to eq(1)
-
-    get_use_case(:amend_baseline).execute(project_id: project_id)
-
-    second_return_id = create_new_return(project_id: project_id, data: initial_return[:data])
-    
-    return_data = get_use_case(:get_return).execute(id: second_return_id)
-    expect(return_data[:baseline_version]).to eq(2)
   end
 
-  it 'should keep track of LAAC Returns' do
-    base_return = get_use_case(:get_base_return).execute(project_id: ac_project_id)
+  let(:first_return_data) { get_use_case(:get_return).execute(id: return_id) }
+  let(:second_return_data) do
+    second_return_id = create_new_return(project_id: project_id, data: initial_return[:data])
+    get_use_case(:get_return).execute(id: second_return_id)
+  end
 
-    expect(base_return[:base_return][:data][:sites]).to eq(expected_ac_base_return[:sites])
+  def get_return(id:)
+    stub_request(:get, "http://#{pcs_domain}/project/#{id}").to_return(
+      status: 200,
+      body: {
+        ProjectManager: 'Michael',
+        Sponsor: 'MSPC'
+      }.to_json
+    ).with(
+      headers: { 'Authorization' => "Bearer #{api_key}" }
+    )
+
+    get_use_case(:get_return).execute(id: id)
+  end
+
+  def update_return(id:, data:)
+    get_use_case(:update_return).execute(return_id: id, data: data[:data])
+  end
+
+  def submit_return(id:)
+    get_use_case(:submit_return).execute(return_id: id)
+  end
+
+  def create_new_return(return_data)
+    get_use_case(:create_return).execute(return_data)[:id]
   end
 
   def soft_update_return(id:, data:)
@@ -435,5 +322,158 @@ describe 'Performing Return on HIF Project' do
 
   def submit_return(id:)
     get_use_case(:submit_return).execute(return_id: id)
+  end
+
+  def expect_return_with_id_to_equal(id:, expected_return:)
+    found_return = get_return(id: id)
+    expect(found_return[:data]).to eq(expected_return[:data])
+    expect(found_return[:status]).to eq(expected_return[:status])
+    expect(found_return[:updates]).to eq(expected_return[:updates])
+  end
+
+  def expect_return_to_be_submitted(id:)
+    found_return = get_return(id: id)
+    expect(found_return[:status]).to eq('Submitted')
+  end
+
+  before do
+    ENV['PCS'] = 'yes'
+    ENV['PCS_DOMAIN'] = pcs_domain
+    ENV['PCS_SECRET'] = 'Secret 2'
+    ENV['OUTPUTS_FORECAST_TAB'] = 'Yes'
+    ENV['CONFIRMATION_TAB'] = 'Yes'
+    ENV['S151_TAB'] = 'Yes'
+    ENV['RM_MONTHLY_CATCHUP_TAB'] = 'Yes'
+    ENV['MR_REVIEW_TAB'] = 'Yes'
+    ENV['OUTPUTS_ACTUALS_TAB'] = 'Yes'
+    ENV['HIF_RECOVERY_TAB'] = 'Yes'
+  end
+
+  after do
+    ENV['OUTPUTS_FORECAST_TAB'] = nil
+    ENV['CONFIRMATION_TAB'] = nil
+    ENV['PCS_SECRET'] = nil
+    ENV['S151_TAB'] = nil
+    ENV['MR_REVIEW_TAB'] = nil
+    ENV['OUTPUTS_ACTUALS_TAB'] = nil
+    ENV['HIF_RECOVERY_TAB'] = nil
+    ENV['PCS'] = nil
+    ENV['PCS_DOMAIN'] = nil
+  end
+
+  def given_a_new_hif_project
+    project_id
+  end
+
+  def given_a_hif_project_with_an_initial_return
+    project_id
+    return_id
+  end
+
+  def when_you_request_a_base_return
+    base_return
+  end
+
+  def when_the_return_is_updated_twice
+    soft_update_return(id: return_id, data: updated_return_data)
+    soft_update_return(id: return_id, data: updated_return_data)
+  end
+
+  def when_the_return_is_submitted
+    submit_return(id: return_id)
+  end
+
+  def when_i_amend_the_baseline
+    first_return_data
+    get_use_case(:amend_baseline).execute(project_id: project_id)
+    second_return_data
+  end
+
+  def then_a_base_return_for_the_project_is_supplied
+    expect(base_return[:base_return][:data]).to eq(expected_base_return)
+  end
+
+  def then_the_return_contains_past_updates
+    expect_return_with_id_to_equal(
+      id: return_id,
+      expected_return: expected_updated_return
+    )
+  end
+
+  def then_the_base_return_reflects_the_past_submitted_returns
+    expect(base_return[:base_return][:data][:infrastructures]).to eq(expected_second_base_return[:infrastructures])
+  end
+
+  def then_the_versions_of_the_future_returns_are_increased
+    expect(first_return_data[:baseline_version]).to eq(1)
+    expect(second_return_data[:baseline_version]).to eq(2)
+  end
+
+  context 'Given a HIF project' do
+    it 'can get a base return for a project' do
+      given_a_new_hif_project
+      when_you_request_a_base_return
+      then_a_base_return_for_the_project_is_supplied
+    end
+
+    it 'can contain the past updates to returns' do
+      given_a_hif_project_with_an_initial_return
+      when_the_return_is_updated_twice
+      then_the_return_contains_past_updates
+    end
+
+    it 'can keep track of returns' do
+      given_a_hif_project_with_an_initial_return
+      when_the_return_is_updated_twice
+      when_the_return_is_submitted
+      when_you_request_a_base_return
+      then_the_base_return_reflects_the_past_submitted_returns
+    end
+
+    it 'can track new returns baseline version' do
+      given_a_hif_project_with_an_initial_return
+      when_i_amend_the_baseline
+      then_the_versions_of_the_future_returns_are_increased
+    end
+  end
+
+  context 'Given an LAAC project' do
+    let(:ac_project_baseline) do
+      File.open("#{__dir__}/../../fixtures/ac_baseline_core.json") do |f|
+        JSON.parse(
+          f.read,
+          symbolize_names: true
+        )
+      end
+    end
+
+    let(:ac_project_id) do
+      get_use_case(:create_new_project).execute(
+        name: '',
+        type: 'ac',
+        baseline: ac_project_baseline,
+        bid_id: 'AC/MV/6'
+      )[:id]
+    end
+
+    let(:ac_base_return) { get_use_case(:get_base_return).execute(project_id: ac_project_id) }
+
+    def given_a_new_ac_project
+      ac_project_id
+    end
+
+    def when_you_request_an_ac_base_return
+      ac_base_return
+    end
+
+    def then_the_ac_base_return_for_the_project_is_supplied
+      expect(ac_base_return[:base_return][:data][:sites]).to eq(expected_ac_base_return[:sites])
+    end
+
+    it 'can keep track of LAAC Returns' do
+      given_a_new_ac_project
+      when_you_request_an_ac_base_return
+      then_the_ac_base_return_for_the_project_is_supplied
+    end
   end
 end
